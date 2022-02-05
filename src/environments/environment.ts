2e382a4b70286d7927d81d7c16a9fc3e0c9bd7c2
MBlev <mb.lev17@gmail.com>
2022-02-05 17:41:35 -0500

    API url added to enviroment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: "https://hn.algolia.com/api/v1/"
 };
+
+//search_by_date?query=angular&page=0
 
 /*
  * For easier debugging in development mode, you can import the following file
