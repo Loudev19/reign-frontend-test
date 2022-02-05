38293df379c11d9f42069c80c46e8259d9ba143f
MBlev <mb.lev17@gmail.com>
2022-02-05 17:46:11 -0500

    News service and interface created into shared module

---
+++ src/app/shared/interfaces/new.ts
@@ -0,0 +1,7 @@
+export interface New {
+    created_at: Date;
+    story_title: string;
+    story_url: string;
+    author: string;
+    is_favorite: boolean;
+}
---
+++ src/app/shared/services/news.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, map, Observable, of, retry } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { New } from '../interfaces/new';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  private path: string = environment.url
+  
+  constructor(
+    private httpService: HttpClient
+  ) { }
+
+  public getPageNews(framework: string = 'angular', page: number = 0): Observable<New[]> {
+    return this.httpService.get<any>(this.path + 'search_by_date?query='+ framework + '&page=' + page).pipe(
+      retry(1),
+      map( all => all.hits
+        .filter((item: any) => item.created_at && item.story_title && item.story_url && item.author)
+        .map((item: any) => ({
+          ...item,
+          created_at: new Date(item.created_at),
+          is_favorite: false
+        }))
+      ),
+      catchError(()=>of()) 
+    )
+  }
+}
