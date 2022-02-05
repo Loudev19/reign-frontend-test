b69b5842f2e10fc49b3f160ebc4471e2b274141d
MBlev <mb.lev17@gmail.com>
2022-02-05 18:58:57 -0500

    Routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllComponent } from './pages/all/all.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/all', pathMatch: 'full'},
+  {path: 'all', component: AllComponent},
+  {path: 'favorites', component: FavoritesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
