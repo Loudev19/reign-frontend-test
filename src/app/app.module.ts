d6bcff361948d313d87ad2a133e18056115c1686
MBlev <mb.lev17@gmail.com>
2022-02-06 01:21:13 -0500

    Core module added to root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AllComponent } from './pages/all/all.component';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
 
 import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { SharedModule } from "./shared/shared.module";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
