8a87f5332c0e759e0f51c9827905e4ce8659e989
MBlev <mb.lev17@gmail.com>
2022-02-05 19:00:24 -0500

    Created header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { FormatTimePipe } from './pipes/format-time.pipe';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     CardComponent,
-    FormatTimePipe
+    FormatTimePipe,
+    HeaderComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     CardComponent,
+    HeaderComponent,
     FormatTimePipe
   ]
 })
